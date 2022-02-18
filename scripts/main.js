9d3eac8015e06e41dd12edb640c50a3dad360190
Fabricio Ceppi <fabricioceppi@gmail.com>
2022-02-17 22:19:30 -0300

    no error still, let's see when I try to configure the transition between states

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,22 @@
 /* mobile viewport height fix */
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+/* let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`); */
 
 // fix when resizing
-window.addEventListener('resize', () => {
+/* window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+  }); */
 
   /* open menu functionallity */
 
-  const nav = document.querySelector('nav');
+/*   const nav = document.querySelector('nav');
   const openButton = document.getElementById('menu-button');
   const closeButton = document.getElementById('close-menu');
 
   openButton.addEventListener('click', () => nav.classList.add('active'));
-  closeButton.addEventListener('click', () => nav.classList.remove('active'));
+  closeButton.addEventListener('click', () => nav.classList.remove('active')); */
 
   /* Game option display */
 
